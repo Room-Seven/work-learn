197910766c9af191c317d6ef668870166049cc7d
Ron_Tayler <max.urs@yandex.ru>
2021-10-08 22:14:29 +0300

    Небольшой фикс файла конфигураций

--- config.php
+++ config.php
@@ -5,32 +5,31 @@
  * @copyright   2020
  */
 
-
 // HTTP & HTTPS Ссылки
-define('DOMEN', 'work-learn.room-seven.ru')
+const DOMAIN = 'work-learn.room-seven.ru';
 
-define('HTTP_SERVER','http://'.DOMEN.'/');
-define('HTTP_FILE','http://'.DOMEN.'/file');
-define('HTTP_IMAGE','http://'.DOMEN.'/image/');
+const HTTP_SERVER = 'http://'.DOMAIN.'/';
+const HTTP_FILE = 'http://'.DOMAIN.'/file';
+const HTTP_IMAGE = 'http://'.DOMAIN.'/image/';
 
-define('HTTPS_SERVER','https://'.DOMEN.'/');
-define('HTTPS_FILE','https://'.DOMEN.'/file/');
-define('HTTPS_IMAGE','https://'.DOMEN.'/image/');
+const HTTPS_SERVER = 'https://'.DOMAIN.'/';
+const HTTPS_FILE = 'https://'.DOMAIN.'/file/';
+const HTTPS_IMAGE = 'https://'.DOMAIN.'/image/';
 
-// DIR / Директории. Данные скрыты для безопастности
-define('DIR_APP',__DIR__.'/');
-define('DIR_SYSTEM','***');
-define('DIR_STORAGE','***');
-define('DIR_DOCS','***');
-define('DIR_IMAGE','***');
-define('DIR_CACHE_FILE','***');
-define('DIR_CACHE_IMAGE','***');
+// DIR / Директории
+const DIR_APP = __DIR__.'/';
+const DIR_SYSTEM = DIR_APP.'system/';
+const DIR_STORAGE = DIR_APP.'storage/';
+const DIR_DOCS = DIR_STORAGE.'docs/';
+const DIR_IMAGE = DIR_STORAGE.'image/';
+const DIR_CACHE_FILE = DIR_APP.'file/';
+const DIR_CACHE_IMAGE = DIR_APP.'image/';
 
 // DB / База данных. Данные скрыты для безопастности
-define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', '***');
-define('DB_USERNAME', '***');
-define('DB_PASSWORD', '***');
-define('DB_DATABASE', '***');
-define('DB_PORT', '***');
-define('DB_PREFIX', '***');
+const DB_DRIVER = 'mysqli';
+const DB_HOSTNAME = '***';
+const DB_USERNAME = '***';
+const DB_PASSWORD = '***';
+const DB_DATABASE = '***';
+const DB_PORT = '***';
+const DB_PREFIX = '***';
